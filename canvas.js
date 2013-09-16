15c8fe170c2b3473b4be8c2e2f6e60ddad509220
Vikash <rockysingla1@gmail.com>
2013-09-16 11:24:18 +0530

    first

---
+++ canvas.js
@@ -0,0 +1,26 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: manusis
+ * Date: 9/11/13
+ * Time: 6:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+function message()
+{
+    try
+
+    {
+
+        adddlert("Welcome guest!");
+    }
+    catch(err)
+    {
+
+        txt="There was an error on this form";
+        alert(txt);
+
+        txt="fill all the blanks";
+        alert(txt);
+
+    }
+}

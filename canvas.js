e00377577b33f785c647d3bea05ce0f3e6ffd3ee
Vikash <rockysingla1@gmail.com>
2013-09-25 11:54:48 +0530

    signup

--- canvas.js
+++ canvas.js
@@ -1,140 +1,375 @@
-/**
- * Created with JetBrains WebStorm.
- * User: manusis
- * Date: 9/11/13
- * Time: 6:13 PM
- * To change this template use File | Settings | File Templates.
- */
-//function message()
-//{
-//    try
-//
-//    {
-//
-//        adddlert("Welcome guest!");
-//    }
-//    catch(err)
-//    {
-//
-//        txt="There was an error on this form";
-//        alert(txt);
-//
-//        txt="fill all the blanks";
-//        alert(txt);
-//
-//    }
-//}
+var createStatement = "CREATE TABLE IF NOT EXISTS Contacts (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, useremail TEXT,address TEXT,phone INTEGER,gender TEXT,hobbies TEXT)";
 
-//$("#save").click(function (e)
-//{
-//    alert("Name Has Been Saved");
-//    e.preventDefault();
-//    var data = $("#form").serializeArray();
-//    var id = Math.floor(Math.random() * 10000001);
-//
-//    var item = {};
-//    item.first = ["First:", $("#first").val()];
-//    item.last = ["Last:", $("#last").val()];
-//
-//    $.each(data, function (i, obj)
-//    {
-//        localStorage.setItem(id, JSON.stringify(item));
-//
-//    });
-//
-//});
-//
-////Fill with data from localstorage to edit
-//function editItem()
-//{
-//    var value = localStorage.getItem(this.key);
-//    var item = JSON.parse(value);
-//
-//    $("#form").show(function ()
-//    {
-//        $("#userDiv").hide(function ()
-//        {
-//        });
-//    });
-//
-//    $("#first").val(item.first[1]);
-//    $("#last").val(item.last[1]);
-//}
-//
-//
-//
-//
-//
-//
-//function deleteItem()
-//{
-//    var ask = confirm("Are you sure you want to delete this contact?");
-//    if (ask)
-//    {
-//        localStorage.removeItem(this.key);
-//        alert("Contact was deleted!!");
-//        window.location.reload();
-//    }
-//    else
-//    {
-//        alert("Contact was NOT deleted.");
-//    }
-//
-//}
-//
-//$("#clear").click(function (e)
-//{  alert("clear field")
-//    e.preventDefault();
-//    if(localStorage.length === 0)
-//    {
-//        alert("There Is No Info To Clear")
-//    }
-//    else
-//    {
-//        localStorage.clear();
-//        alert("All Names Have Been Deleted!");
-//        window.location.reload();
-//        return false;
-//    }
-//});
+var selectAllStatement = "SELECT * FROM Contacts";
 
+var insertStatement = "INSERT INTO Contacts (username, useremail, address, phone,gender, hobbies) VALUES (?, ?, ?, ?, ?, ?)";
 
+var updateStatement = "UPDATE Contacts SET username = ?, useremail = ?, address = ?, phone = ?,gender = ?,hobbies = ? WHERE id=?";
 
-$(document).ready(function()
+var deleteStatement = "DELETE FROM Contacts WHERE id=?";
+
+var dropStatement = "DROP TABLE Contacts";
+
+var db = openDatabase("AddressBook", "1.0", "Address Book", 200000);  // Open SQLite Database
+
+var dataset;
+
+var DataType;
+
+function initDatabase()  // Function Call When Page is ready.
+
 {
-    if (!window.localStorage)
-    {
-        alert('Your browser does not support HTML5 localStorage. Try upgrading.');
-    } else
-    {
-        $("#return_form").submit(function()
+
+    try {
+
+        if (!window.openDatabase)  // Check browser is supported SQLite or not.
+
         {
-            setAllItems();
-        });
-    }
-});
 
-var setAllItems = function()
+            alert('Databases are not supported in this browser.');
+
+        }
+
+        else {
+
+            createTable();  // If supported then call Function for create table in SQLite
+
+        }
+
+    }
+
+    catch (e) {
+
+        if (e == 2) {
+
+            // Version number mismatch.
+
+            console.log("Invalid database version.");
+
+        } else {
+
+            console.log("Unknown error " + e + ".");
+
+        }
+
+        return false;
+
+    }
+
+}
+
+function createTable()  // Function for Create Table in SQLite.
+
 {
 
-    var newDate, itemId, formSave;
+    db.transaction(function (tx) {
+        tx.executeSql(createStatement, [], showRecords, onError);
+    });
 
-    newDate = new Date();
-    itemId = newDate.getTime();
-    formSave =
-    {
-        fname											: $("input[name='first']").val(),
-        lname 											: $("input[name='last']").val(),
-        email 											: $("input[name='email']").val(),
-//        email 											: $("input[name='cemail']").val(),
-       password											: $("input[name='password']").val(),
-//       password									      : $("select[name='cpassword']").val(),
-       date								                : $("input[name='date']").val(),
-       gender							                : $("select[name='gender']").val(),
-       checkbox							                : $("input[name='check']").attr('checked')
+}
 
-    };
 
-    // turn data into JSON string
-    localStorage.setItem( itemId, JSON.stringify(formSave));
-};
+function deleteRecord(id) // Get id of record . Function Call when Delete Button Click..
+
+{
+
+    var iddelete = id.toString();
+
+    db.transaction(function (tx) {
+        tx.executeSql(deleteStatement, [id], showRecords, onError);
+        alert("Deleted record Sucessfully");
+    });
+
+    resetForm();
+
+}
+
+function updateRecord() // Get id of record . Function Call when Delete Button Click..
+
+{
+
+
+}
+
+function dropTable() // Function Call when Drop Button Click.. Table will be dropped from database.
+
+{
+//       console.log("dropTable()");
+    db.transaction(function (tx) {
+        tx.executeSql(dropStatement, [], showRecords, onError);
+    });
+
+    resetForm();
+
+    initDatabase();
+
+}
+
+function loadRecord(i) // Function for display records which are retrived from database.
+
+{
+
+    var item = dataset.item(i);
+    console.log("item");
+
+    $("#username").val((item['username']).toString());
+
+    $("#useremail").val((item['useremail']).toString());
+
+    $("#address").val((item['address']).toString());
+
+    $("#phone").val((item['phone']).toString());
+
+
+    $("#id").val((item['id']).toString());
+    console.log(item);
+}
+
+function resetForm() // Function for reset form input values.
+
+{
+
+    $("#username").val("");
+
+    $("#useremail").val("");
+
+    $("#address").val("");
+
+    $("#phone").val("");
+
+    $("#gender").val("");
+
+    $('input[type=checkbox]').attr('checked', false);
+
+
+
+    $("#id").val("");
+
+}
+
+function loadAndReset() //Function for Load and Reset...
+
+{
+
+    resetForm();
+
+    showRecords();
+
+
+}
+
+function onError( error) // Function for Hendeling Error...
+
+{
+
+    alert(error.message);
+
+}
+
+function showRecords() // Function For Retrive data from Database Display records as list
+
+{
+
+    $("#results").html('');
+
+    db.transaction(function (tx) {
+
+        tx.executeSql(selectAllStatement, [], function (tx, result) {
+
+            dataset = result.rows;
+
+            for (var i = 0, item = null; i < dataset.length; i++) {
+
+                item = dataset.item(i);
+
+                var linkeditdelete = '<tr><td><li>' + item['username'] + ' , ' + item['useremail'] + ' , ' + item['address'] + ' , ' + item['phone'] + ' , ' + '<a href="#" onclick="loadRecord(' + i + ');">edit</a>' + '    ' +
+
+                    '<a href="#" onclick="deleteRecord(' + item['id'] + ');">delete</a></li></td></tr>';
+
+                $("#results").append(linkeditdelete);
+
+            }
+            createOption();
+
+        });
+
+    });
+
+}
+//function handleFileSelect(evt) {
+//    console.log('inside event');
+//    //   alert("hello");
+//    var files = evt.target.files; // FileList object
+//    // Loop through the FileList and render image files as thumbnails.
+//    for (var i = 0, f; f = files[i]; i++) {
+////            alert(f);
+////        // Only process image files.
+////        if (!f.type.match('image.*')) {
+////            continue;
+////        }
+//
+//
+//        var reader = new FileReader();
+//
+//        reader.onload = function (evt) {
+//            $('#preview').attr('src', evt.target.result);
+//        }
+//        // Closure to capture the file information.
+//        reader.onload = (function (f) {
+//            return function (e) {
+//                // Render thumbnail.
+//
+////                var span = document.createElement('span');
+////                span.innerHTML = ['<img class="thumb" src="', e.target.result,
+////                    '" title="', escape(theFile.name), '"/>'].join('');
+////                document.getElementById('list').insertBefore(span, null);
+//
+////                var imagetemp = e.target.result;
+////                $('#image').attr('target', imagetemp);
+//                //   db.transaction(function (tx) { tx.executeSql(insertStatement, [imagetemp],onError); });
+//
+//                // function insertRecord() // Get value from Input and insert record . Function Call when Save/Submit Button Click..
+//
+//                //{
+//
+//                // var imagetemp = $('input:file[id=image]').val();
+//
+//
+//                //tx.executeSql(SQL Query Statement,[ Parameters ] , Sucess Result Handler Function, Error Result Handler Function );
+//
+//                //}
+//
+//
+//            };
+//        })(f);
+//
+//        // Read in the image file as a data URL.
+//        reader.readAsDataURL(f);
+//    }
+//}
+//$('#image').change(function (event) {
+//
+//    handleFileSelect(event);
+//
+//    $('#preview').attr('src',$('#image').attr('target'));
+//});
+
+function initialLoad() {
+
+
+    $("body").fadeIn(2000); // Fede In Effect when Page Load..
+
+    initDatabase();
+
+
+    $(document).on('change', '#color', function () {
+        // fires only after clicking OK
+        $('#mycontact').css('background-color', $(this).val());
+    });
+
+    $(document).on('change', '#detail', function () {
+
+
+      console.log(dataset);
+        for (var i = 0, item = null; i < dataset.length; i++) {
+            item = dataset.item(i);
+            if (item.id == $(this).val()) {
+                alert("welcome  :" + item.username + " " + item.phone + " ");
+                // console.log(item);
+            }
+
+        }
+    });
+
+    $(document).on('click', '#submitButton', function () {
+        alert("in here");
+        var usernametemp = $('input:text[id=username]').val();
+
+        var useremailtemp = $('input:text[id=useremail]').val();
+
+        var useraddresstemp = $('input:text[id=address]').val();
+
+        var phonetemp = $('input:text[id=phone]').val();
+
+        var gender = $('input[name=gender]:checked').val();
+        var hobbies = [];
+        $('input[name=hobbies]:checked').each(function () {
+            hobbies.push($(this).val());
+        });
+//        var imagetemp = $('#image').attr('target');
+        db.transaction(function (tx) {
+            tx.executeSql(insertStatement, [usernametemp, useremailtemp, useraddresstemp, phonetemp, gender, hobbies], loadAndReset,showRecords(), onError);
+        });
+        return false;
+    });
+
+    $(document).on('click', '#btnUpdate', function () {
+
+
+        console.log("");
+
+        var usernameupdate = $('input:text[id=username]').val().toString();
+
+        var useremailupdate = $('input:text[id=useremail]').val().toString();
+
+        var addressupdate = $('input:text[id=address]').val().toString();
+
+        var phoneupdate = $('input:text[id=phone]').val().toString();
+
+//        var imageupdate = $('#image').attr('target');
+
+        var gender = $('input[name=gender]:checked').val();
+        var hobbies = [];
+        $('input[name=hobbies]:checked').each(function () {
+            hobbies.push($(this).val());
+
+    });
+//        console.log(hobbies);
+//        console.log(gender);
+//        console.log(phoneupdate);
+
+        var useridupdate = $("#id").val();
+        // db.transaction(function (ts){ ts.executeSql(deletes,[],loadAndReset(),onError())})
+        db.transaction(function (tx) {
+            tx.executeSql(updateStatement, [usernameupdate, useremailupdate, addressupdate, phoneupdate, gender, hobbies, useridupdate], loadAndReset,showRecords(), onError);
+        });
+
+
+    });
+
+
+// $("#submitButton").bind('click',alert("hello"));  // Register Event Listener when button click.
+//
+//      $("#btnUpdate").click(updateRecord);
+
+    $("#btnReset").click(resetForm);
+
+    $("#btnDrop").click(dropTable);
+
+
+}
+
+function createOption() {
+
+    db.transaction(function (tx) {
+
+        tx.executeSql(selectAllStatement, [], function (tx, result) {
+
+            dataset = result.rows;
+
+            for (var i = 0, item = null; i < dataset.length; i++) {
+
+                item = dataset.item(i);
+
+                var opt = new Option(item.id, item.id);
+                //   $(opt).html(item.id);
+
+                $("#detail").append(opt);
+
+            }
+
+        });
+
+    });
+
+}
--- create acc.js
+++ create acc.js
@@ -1,12 +1,14 @@
 var createStatement = "CREATE TABLE IF NOT EXISTS Contacts (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT,lastname TEXT, useremail TEXT,usercemail TEXT,password TEXT,cpassword TEXT,date INTEGER,gender TEXT)";
 
-//var selectAllStatement = "SELECT * FROM Contacts";
+var selectAllStatement = "SELECT * FROM Contacts ";
 
 var insertStatement = "INSERT INTO Contacts (username,lastname, useremail,usercemail, password,cpassword,date,gender) VALUES (?, ?, ?, ?, ?,?,?,?)";
-var updateStatement = "UPDATE Contacts SET username = ?,lastname = ?, useremail = ?,usercemail = ?, password = ?,cpassword = ? date = ?,gender = ?,WHERE ID = ?";
+var updateStatement = "UPDATE Contacts SET username = ?,lastname = ?, useremail = ?,usercemail = ?, password = ?,cpassword = ? date = ?,gender = ?, WHERE id = ?";
+var deleteStatement = "DELETE FROM Contacts WHERE id=?";
+var dropStatement = "DROP TABLE Contacts";
 var db = openDatabase("AddressBook", "1.0", "Address Book", 200000);  // Open SQLite Database
 
-//var dataset;
+var dataset = {};
 
 //var DataType;
 
@@ -46,7 +48,7 @@ function initDatabase()  // Function Call When Page is ready.
 
         }
 
-        return;
+        return false;
 
     }
 
@@ -57,13 +59,51 @@ function createTable()  // Function for Create Table in SQLite.
 {
 
     db.transaction(function (tx) {
-        tx.executeSql(createStatement, [], onError);
+        tx.executeSql(createStatement, [],showRecords(), onError);
 //        console.log(tx);
 
     });
-
+    resetForm();
 }
 createTable();
+function deleteRecord(id)
+{
+ var iddelete = id.toString();
+    db.transaction(function(tx){
+     tx.executeSql(deleteStatement,[],showRecords(),onError());
+        alert('Deleted record sucessfully');
+    });
+}
+function dropTable()
+{
+    alert("delete all data");
+    db.transaction(function(tx){
+     tx.executeSql(dropStatement,[],showRecords(),onError);
+    });
+    resetForm();
+    initDatabase();
+}
+function resetForm()
+
+{
+    console.log("===");
+ $("#username").val("");
+ $("#lastname").val("");
+ $("#useremail").val("");
+ $("#address").val("");
+ $("#caddress").val("");
+ $("#phone").val("");
+ $("#gender").val("");
+ $("#id").val("");
+}
+
+
+function loadandreset()
+{
+    resetForm();
+    showRecords();
+
+}
 
 
 function onError(error) // Function for Hendeling Error...
@@ -73,69 +113,212 @@ function onError(error) // Function for Hendeling Error...
     alert(error.message);
 
 }
+function showRecords() // Function For Retrive data from Database Display records as list
 
-
-$(document).on('click', '#submitButton', function () {
-
-
-    var usernametemp = $('#username').val();
-
-    var lastnametemp = $('#lastname').val();
-
-    var useremailtemp = $('#useremail').val();
-
-    var usercemailtemp = $('#usercemail').val();
-
-    var userpasswordtemp = $('#address').val();
-
-    var usercpasswordtemp = $('#caddress').val();
-
-    var datetemp = $('#phone').val();
-
-    var gender = $("#gender").val();
-
-    console.log(datetemp);
-    console.log(usernametemp);
-    console.log(useremailtemp);
-    console.log(usercemailtemp);
-    console.log(userpasswordtemp);
-    console.log(usercpasswordtemp);
-    console.log(gender);
-    console.log(lastnametemp);
-
-//    var hobbies = [];
-//    $('input[name=hobbies]:checked').each(function () {
-//        hobbies.push($(this).val());
-//    });
-//    var imagetemp = $('#image').attr('target');
-    db.transaction(function (tx) {
-        tx.executeSql(insertStatement, [usernametemp, lastnametemp, useremailtemp,usercemailtemp, userpasswordtemp,usercpasswordtemp, datetemp, gender], onError);
-//       console.log(tx);
-    });
-    return false;
-});
-$(document).on('click','#updatebutton',function()
 {
-    var usernametemp = $('#username').val();
 
-    var lastnametemp = $('#lastname').val();
+    $("#results").html('');
 
-    var useremailtemp = $('#useremail').val();
-
-    var usercemailtemp = $('#usercemail').val();
-
-    var userpasswordtemp = $('#address').val();
-
-    var usercpasswordtemp = $('#caddress').val();
-
-    var datetemp = $('#phone').val();
-
-    var gender = $("#gender").val();
-    $("#id").val((item['id']).toString());
-    var useridupdate = $("#id").val();
     db.transaction(function (tx) {
-        tx.executeSql(updateStatement, [usernametemp, lastnametemp, useremailtemp,usercemailtemp, userpasswordtemp,usercpasswordtemp, datetemp, gender], onError);
-//       console.log(tx);
+
+        tx.executeSql(selectAllStatement, [], function (tx, result)
+
+        {
+
+            dataset = result.rows;
+//            console.log(result);
+
+            for (var i = 0, item = null; i < dataset.length; i++)
+            {
+
+                item = dataset.item(i);
+
+                var linkeditdelete = '<tr><td><li>' + item['username'] + ' , '+item['lastname'] + ' , ' + item['useremail'] + ' , '+ item['usercemail'] + ' , ' + item['password'] + ' , ' + item['cpassword'] + ' , ' + item['date'] + ' , ' + item['gender'] + ' , ' + '"/>' + '    ' +
+
+                    '<a href="#" onclick="deleteRecord(' + item['id'] + ');">delete</a></li></td></tr>';
+
+                $("#results").append(linkeditdelete);
+
+            }
+            createOption();
+
+        });
+
     });
-    return false;
+
+}
+
+
+function loadRecord(id) // Function for display records which are retrived from database.
+
+{
+  console.log(loadRecord());
+    $("#submitButton").attr("disabled", "disabled");
+    $("#Updatebutton").removeAttr("disabled");
+
+
+    var item = dataset.item(id);
+
+//      console.log(item) ;
+    $("#username").val((item['username']).toString());
+
+    $("#lastname").val((item['lastname']).toString());
+
+    $("#useremail").val((item['useremail']).toString());
+
+    $("#usercemail").val((item['usercemail']).toString());
+
+    $("#address").val((item['password']).toString());
+
+    $("#caddress").val((item['cpassword']).toString());
+
+    $("#phone").val((item['date']).toString());
+
+    $("#gender").val((item['gender']).toString());
+
+
+
+
+    $("#id").val((item['id']).toString());
+
+    tx.executeSql(updateStatement, [item],onError);
+//       console.log(tx);
+
+
+}
+
+
+function initialLode()
+{
+    $("body").fadeIn(2000);
+    initDatabase();
+    createTable();
+//    $(document).on('change','#color',function(){
+//       $("mycontact").css('background color',$(this).val());
+//    }) ;
+
+    $(document).on('change','#detail',function(){
+        console.log("==========");
+        for(var i = 0, item = null;i<dataset.length; i++){
+            item = dataset.item(i) ;
+            if(item.id==$(this).val()){
+           alert('welcome' +item.username + "" + item.date + "");
+
+        }
+        }
+
 });
+    $(document).on('click', '#submitButton', function ()
+    {
+         alert("submit all data");
+        var usernametemp = $('#username').val();
+
+        var lastnametemp = $('#lastname').val();
+
+        var useremailtemp = $('#useremail').val();
+
+        var usercemailtemp = $('#usercemail').val();
+
+        var userpasswordtemp = $('#address').val();
+
+        var usercpasswordtemp = $('#caddress').val();
+
+        var datetemp = $('#phone').val();
+
+        var gender = $("#gender").val();
+//        var useridsubmit = $("id").val();
+
+        console.log(datetemp);
+        console.log(usernametemp);
+        console.log(useremailtemp);
+        console.log(usercemailtemp);
+        console.log(userpasswordtemp);
+        console.log(usercpasswordtemp);
+        console.log(gender);
+        console.log(lastnametemp);
+//        console.log(useridsubmit);
+
+
+   db.transaction(function (tx){
+            tx.executeSql(insertStatement, [usernametemp, lastnametemp, useremailtemp,usercemailtemp, userpasswordtemp,usercpasswordtemp, datetemp, gender],loadandreset(),onError);
+//       console.log(tx);
+        });
+
+        return false;
+    });
+
+
+    $(document).on('click','#updatebutton',function()
+
+    {
+
+//    console.log('=================');
+        var usernametemp = $('#username').val();
+
+        var lastnametemp = $('#lastname').val();
+
+        var useremailtemp = $('#useremail').val();
+
+        var usercemailtemp = $('#usercemail').val();
+
+        var userpasswordtemp = $('#address').val();
+
+        var usercpasswordtemp = $('#caddress').val();
+
+        var datetemp = $('#phone').val();
+
+        var gender = $("#gender").val();
+
+        console.log(datetemp);
+        console.log(usernametemp);
+        console.log(useremailtemp);
+        console.log(usercemailtemp);
+        console.log(userpasswordtemp);
+        console.log(usercpasswordtemp);
+        console.log(gender);
+        console.log(lastnametemp);
+//    $("#id").val((item['id']).toString());
+        var useridupdate = $("#id").val();
+
+        console.log(useridupdate);
+
+        db.transaction(function (tx)
+        {
+            tx.executeSql(updateStatement, [usernametemp, lastnametemp, useremailtemp,usercemailtemp, userpasswordtemp,usercpasswordtemp, datetemp, gender,useridupdate],loadandreset(),onError);
+//      console.log(tx);
+        });
+        return false;
+    });
+//    $("#btnUpdate").click(updateRecord);
+
+    $("#btnReset").click(resetForm);
+
+    $("#btnDrop").click(dropTable);
+    return false;
+
+
+}
+function createOption() {
+
+    db.transaction(function (tx) {
+
+        tx.executeSql(selectAllStatement, [], function (tx, result) {
+
+            dataset = result.rows;
+
+            for (var i = 0, item = null; i < dataset.length; i++) {
+
+                item = dataset.item(i);
+
+                var opt = new Option(item.id, item.id);
+                //   $(opt).html(item.id);
+
+                $("#detail").append(opt);
+
+            }
+
+        });
+
+    });
+
+}
